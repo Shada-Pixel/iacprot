de919c65445c35f254390f92e4e1398c2ba720f0
Hussains3 <hussainasabbir3@gmail.com>
2023-05-08 19:19:50 +0600

    Complete before content

--- src/main.js
+++ src/main.js
@@ -1,64 +1,102 @@
-$(".project-carousel").owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: false,
-    dots: false,
-    autoplay: true,
-    responsive: {
-        0: {
-            items: 1,
-        },
-        600: {
-            items: 2,
-        },
-        1000: {
-            items: 3,
-        },
-    },
-});
+$(document).ready(function() {
 
 
-$(".testimonial-carousel").owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: false,
-    dots: true,
-    autoplay: true,
-    responsive: {
-        0: {
-            items: 1,
-        },
-        600: {
-            items: 1,
-        },
-        1000: {
-            items: 1,
-        },
-    },
-});
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 70) {
+        $('.header').removeClass('bg-transparent');
+        $('.header').addClass('bg-white/80');
+      } else {
+        $('.header').addClass('bg-transparent');
+        $('.header').removeClass('bg-white/80');
+      } 
 
-$(".counter").each(function () {
-    var $this = $(this),
-        countTo = $this.attr("data-count");
+      console.log($(this).scrollTop());
+    });
 
-    $({ countNum: $this.text() }).animate(
-        {
-            countNum: countTo,
-        },
 
-        {
-            duration: 8000,
-            easing: "linear",
-            step: function () {
-                $this.text(Math.floor(this.countNum));
+    // project carousel
+    $(".project-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: false,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1,
             },
-            complete: function () {
-                $this.text(this.countNum);
-                //alert('finished');
+            600: {
+                items: 2,
             },
-        }
-    );
-});
+            1000: {
+                items: 3,
+            },
+        },
+    });
+    // testimonial carousel
+    $(".testimonial-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: true,
+        autoplay: true,
+        responsive: {
+            0: {
+                items: 1,
+            },
+            600: {
+                items: 1,
+            },
+            1000: {
+                items: 1,
+            },
+        },
+    });
+    // countup
+    $(".counter").each(function () {
+        var $this = $(this),
+            countTo = $this.attr("data-count");
+    
+        $({ countNum: $this.text() }).animate(
+            {
+                countNum: countTo,
+            },
+    
+            {
+                duration: 8000,
+                easing: "linear",
+                step: function () {
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function () {
+                    $this.text(this.countNum);
+                    //alert('finished');
+                },
+            }
+        );
+    });
+
+    $(".popmenutoggle.burger").click(function () {
+        $(".popmenu").toggleClass('hidden');
+        $(".popmenutoggle.burger").toggleClass('hidden inline-flex');
+    });
+    $(".popmenutoggle.cross").click(function () {
+        $(".popmenu").toggleClass('hidden');
+        $(".popmenutoggle.burger").toggleClass('hidden inline-flex');
+    });
+
+
+    // contact pop
+    $(".contact-poptoggle").click(function () {
+        $(".contac").toggleClass('hidden');
+    });
+
+
+
+
+
+    
+  });
 
 
 
