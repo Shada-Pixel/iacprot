b5898f53ac9cecf6741418e3aa72526a68034e6e
Hussains3 <hussainasabbir3@gmail.com>
2023-05-09 15:34:39 +0600

    Slider Fixed

--- src/main.js
+++ src/main.js
@@ -14,23 +14,27 @@ $(document).ready(function () {
 
     // project carousel
     $(".project-carousel").owlCarousel({
-        loop: true,
-        margin: 10,
-        nav: false,
-        dots: false,
-        autoplay: true,
-        responsive: {
-            0: {
-                items: 1,
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplaySpeed: 1000,
+        dots:false,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
             },
-            600: {
-                items: 2,
+            600:{
+                items:2,
+                nav:false
             },
-            1000: {
-                items: 3,
-            },
-        },
-    });
+            1000:{
+                items:3,
+                nav:false
+            }
+        }
+    })
     // testimonial carousel
     $(".testimonial-carousel").owlCarousel({
         loop: true,
